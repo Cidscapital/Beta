8b9e736b9b730cb047bf1ebebc6064d648c8ada6
Cidscapital <72131219+Cidscapital@users.noreply.github.com>
2021-03-22 20:04:30 +0300

    Add files via upload

---
+++ mycart.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<div style="border: 1px green; background-color: #f2f2f2; border-radius: 5px; paint-order: green;">
+		<h2>
+Dear <?php echo $_POST["name"]; ?><br>
+You have successfully booked table number: <?php echo $_POST["tnum"]; ?><br>
+As you will be taking the following listed meal: <?php echo $_POST["food"]; ?><br>
+		</h2>
+</div>
+</body>
+</html>
